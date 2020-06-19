d381e526b3906c2aa2fca6603cc1a19729a3b764
Daniel <sittingbout@gmail.com>
2020-06-19 10:13:57 +0200

    opcionesActivities test creado

---
+++ app/src/test/java/com/example/test/OpcionesActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.test;
+import com.example.tetris.OpcionesActivity;
+
+import org.junit.Test;
+public class OpcionesActivityTest {
+
+    OpcionesActivity opcionesActivity;
+
+    @Test
+    public void SetColorTest(){
+        opcionesActivity.
+    }
+
+}
